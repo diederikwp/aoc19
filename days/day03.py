3ad9ce8d9057d7b72bf37cbae87f2fb3474bebd5
Diederik Perdok <diederikperdok@planet.nl>
2019-12-07 17:58:02 +0100

    Solve day 3 part 2

--- days/day03.py
+++ days/day03.py
@@ -1,72 +1,115 @@
-from collections import namedtuple
-
-
-LineSegment = namedtuple('LineSegment', ['from_x', 'from_y', 'to_x', 'to_y'])
-
-
 def parse_puzzle_input(puzzle_input):
     lines = puzzle_input.splitlines()
-    return lines[0].split(','), lines[1].split(',')
+    return Wire(lines[0].split(',')), Wire(lines[1].split(','))
 
 
-def trace_line_segments(line_path):
-    hor_segments = []
-    vert_segments = []
-    x = 0
-    y = 0
+class LineSegment:
+    def __init__(self, from_x, from_y, to_x, to_y):
+        self.from_x = from_x
+        self.to_x = to_x
+        self.from_y = from_y
+        self.to_y = to_y
 
-    for step in line_path:
-        direction = step[0]
-        distance = int(step[1:])
+        self.min_x = min(from_x, to_x)
+        self.max_x = max(from_x, to_x)
+        self.min_y = min(from_y, to_y)
+        self.max_y = max(from_y, to_y)
 
-        if direction == 'U':
-            segment = LineSegment(from_x=x, from_y=y, to_x=x, to_y=y + distance)
-            vert_segments.append(segment)
-            y += distance
-        elif direction == 'D':
-            # Direction does not matter. Always having "from <= to" makes things easier.
-            segment = LineSegment(from_x=x, from_y=y - distance, to_x=x, to_y=y)
-            vert_segments.append(segment)
-            y -= distance
-        elif direction == 'R':
-            segment = LineSegment(from_x=x, from_y=y, to_x=x + distance, to_y=y)
-            hor_segments.append(segment)
-            x += distance
-        elif direction == 'L':
-            # Direction does not matter. Always having "from <= to" makes things easier.
-            segment = LineSegment(from_x=x - distance, from_y=y, to_x=x, to_y=y)
-            hor_segments.append(segment)
-            x -= distance
+        self.length = self.max_x - self.min_x + self.max_y - self.min_y
 
-    return hor_segments, vert_segments
+    def __eq__(self, other):
+        return self.from_x == other.from_x and self.to_x == other.to_x and self.from_y == other.from_y
 
+    def __repr__(self):
+        return f'LineSegment from ({self.from_x}, {self.from_y}) to ({self.to_x}, {self.to_y})'
 
-def find_intersections(hor_segments, vert_segments):
-    # Possible performance improvement:
-    #   - sort the horizontal segments by 'from_y' and the vertical segments by 'from_x'
-    #   - for cur_segment in the shorter list:
-    #       - find the first element in the other list just left / just below cur_segment using bisection search
-    #       - start looping over the other list from here until you are just right / just above cur_segment
-    #       - check for intersections in each iteration
+    def intersects(self, other):
+        if self.min_x >= other.max_x:
+            return False
+        if self.max_x <= other.min_x:
+            return False
+        if self.min_y >= other.max_y:
+            return False
+        if self.max_y <= other.min_y:
+            return False
 
-    intersections = []
-    for hor_segment in hor_segments:
-        for vert_segment in vert_segments:
-            if hor_segment.from_x < vert_segment.from_x < hor_segment.to_x and \
-                    vert_segment.from_y < hor_segment.from_y < vert_segment.to_y:
-                intersections.append((vert_segment.from_x, hor_segment.from_y))
+        return True
 
-    return intersections
+    def is_horizontal(self):
+        return self.from_y == self.to_y
 
 
-def manhattan(coords):
-    return [abs(c[0]) + abs(c[1]) for c in coords]
+class Wire:
+    def __init__(self, path_description):
+        self.vertices = [(0, 0)]
+        self.segments = []
+
+        x = 0
+        y = 0
+        total_distance = 0
+        for step in path_description:
+            direction = step[0]
+            distance = int(step[1:])
+
+            if direction == 'U':
+                self.segments.append(LineSegment(from_x=x, from_y=y, to_x=x, to_y=y + distance))
+                y += distance
+            elif direction == 'D':
+                self.segments.append(LineSegment(from_x=x, from_y=y, to_x=x, to_y=y - distance))
+                y -= distance
+            elif direction == 'R':
+                self.segments.append(LineSegment(from_x=x, from_y=y, to_x=x + distance, to_y=y))
+                x += distance
+            elif direction == 'L':
+                self.segments.append(LineSegment(from_x=x, from_y=y, to_x=x - distance, to_y=y))
+                x -= distance
+
+            self.vertices.append((x, y))
+            total_distance += distance
+
+    def __eq__(self, other):
+        return self.vertices == other.vertices and self.segments == other.segments
+
+    def distance_to(self, x, y):
+        distance = 0
+        for seg in self.segments:
+            if seg.is_horizontal():
+                if y == seg.from_y and seg.min_x <= x <= seg.max_x:
+                    return distance + abs(x - seg.from_x)
+            else:
+                if x == seg.from_x and seg.min_y <= y <= seg.max_y:
+                    return distance + abs(y - seg.from_y)
+
+            distance += seg.length
+
+        return None
+
+    def intersections(self, other):
+        # This takes O(nm) time, where n is number of self segments and m is number of other segments. Performance can
+        # be improved by sorting horizontal segments by 'from_y' and vertical segments by 'from_x'. Sorting will take
+        # O(n log(n)). Then a list of other segments that might intersect can be found using bisection search, in
+        # O(log (n)).
+        crossings = set()
+        for ss in self.segments:
+            for os in other.segments:
+                if ss.intersects(os):
+                    if ss.is_horizontal():
+                        crossings.add((os.from_x, ss.from_y))
+                    else:
+                        crossings.add((ss.from_x, os.from_y))
+
+        return crossings
 
 
 def solve_part_1(puzzle_input):
-    line_paths = parse_puzzle_input(puzzle_input)
-    hor_segm_1, vert_segm_1 = trace_line_segments(line_paths[0])
-    hor_segm_2, vert_segm_2 = trace_line_segments(line_paths[1])
+    wire_1, wire_2 = parse_puzzle_input(puzzle_input)
 
-    intersections = find_intersections(hor_segm_1, vert_segm_2) + find_intersections(hor_segm_2, vert_segm_1)
-    return sorted(manhattan(intersections))[0]
+    intersections = wire_1.intersections(wire_2)
+    return min([abs(x) + abs(y) for x, y in intersections])
+
+
+def solve_part_2(puzzle_input):
+    wire_1, wire_2 = parse_puzzle_input(puzzle_input)
+
+    intersections = wire_1.intersections(wire_2)
+    return min([wire_1.distance_to(x, y) + wire_2.distance_to(x, y) for x, y in intersections])
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -1,48 +1,101 @@
 import unittest
 from pathlib import Path
 
-from days.day03 import LineSegment, parse_puzzle_input, trace_line_segments, find_intersections, solve_part_1
+from days.day03 import LineSegment, Wire, parse_puzzle_input, solve_part_1, solve_part_2
+
+
+class TestLineSegment(unittest.TestCase):
+    def setUp(self):
+        self.ls1 = LineSegment(2, 1, -2, 1)
+        self.ls2 = LineSegment(2, -3, 2, 1)
+        self.ls3 = LineSegment(3, -2, -1, -2)
+
+    def test_init(self):
+        self.assertEqual(self.ls1.min_x, -2)
+        self.assertEqual(self.ls1.max_x, 2)
+        self.assertEqual(self.ls1.min_y, 1)
+        self.assertEqual(self.ls1.max_y, 1)
+        self.assertEqual(self.ls1.length, 4)
+
+    def test_intersects(self):
+        self.assertFalse(self.ls1.intersects(self.ls2))
+        self.assertFalse(self.ls2.intersects(self.ls1))
+
+        self.assertFalse(self.ls1.intersects(self.ls3))
+        self.assertFalse(self.ls3.intersects(self.ls1))
+
+        self.assertTrue(self.ls2.intersects(self.ls3))
+        self.assertTrue(self.ls3.intersects(self.ls2))
+
+    def test_is_horizontal(self):
+        self.assertTrue(self.ls1.is_horizontal())
+        self.assertFalse(self.ls2.is_horizontal())
+        self.assertTrue(self.ls3.is_horizontal())
+
+
+class TestWire(unittest.TestCase):
+    def setUp(self):
+        self.wire1 = Wire(['R8', 'U5', 'L5', 'D3'])
+        self.wire2 = Wire(['U7', 'R6', 'D4', 'L4'])
+        self.wire3 = Wire(['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'])
+        self.wire4 = Wire(['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'])
+
+    def test_line_segments(self):
+        self.assertEqual(self.wire3.segments, [LineSegment(0, 0, 75, 0),
+                                               LineSegment(75, 0, 75, -30),
+                                               LineSegment(75, -30, 158, -30),
+                                               LineSegment(158, -30, 158, 53),
+                                               LineSegment(158, 53, 146, 53),
+                                               LineSegment(146, 53, 146, 4),
+                                               LineSegment(146, 4, 217, 4),
+                                               LineSegment(217, 4, 217, 11),
+                                               LineSegment(217, 11, 145, 11)])
+
+    def test_vertices(self):
+        self.assertEqual(self.wire3.vertices, [(0, 0), (75, 0), (75, -30), (158, -30), (158, 53), (146, 53), (146, 4),
+                                               (217, 4), (217, 11), (145, 11)])
+
+    def test_distance_to(self):
+        self.assertEqual(self.wire3.distance_to(146, 18), 318)
+
+    def test_intersections(self):
+        intersections = {(6, 5), (3, 3)}
+
+        self.assertEqual(self.wire1.intersections(self.wire2), intersections)
+        self.assertEqual(self.wire2.intersections(self.wire1), intersections)
 
 
 class TestDay03(unittest.TestCase):
+    def setUp(self):
+        self.test_input_1 = Path('tests/input/test_input_day03_01.txt').read_text()
+        self.test_input_2 = Path('tests/input/test_input_day03_02.txt').read_text()
+        self.test_input_3 = Path('tests/input/test_input_day03_03.txt').read_text()
+
     def test_parse_puzzle_input(self):
         puzzle_input = 'R75,D30,R83,U83,L12,D49,R71,U7,L72\n' \
                        + 'U62,R66,U55,R34,D71,R55,D58,R83'
 
-        parsed1, parsed2 = parse_puzzle_input(puzzle_input)
-        self.assertEqual(parsed1, ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'])
-        self.assertEqual(parsed2, ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'])
-
-    def test_trace_line_segments(self):
-        segments_hor, segments_vert = trace_line_segments(['U62', 'R66', 'U55', 'R34', 'D71', 'L55', 'D58', 'R83'])
-
-        self.assertEqual(segments_vert, [LineSegment(0, 0, 0, 62),
-                                         LineSegment(66, 62, 66, 117),
-                                         LineSegment(100, 46, 100, 117),
-                                         LineSegment(45, -12, 45, 46)])
-
-        self.assertEqual(segments_hor, [LineSegment(0, 62, 66, 62),
-                                        LineSegment(66, 117, 100, 117),
-                                        LineSegment(45, 46, 100, 46),
-                                        LineSegment(45, -12, 128, -12)])
-
-    def test_find_intersections(self):
-        hor_segments = [LineSegment(0, 0, 8, 0), LineSegment(3, 5, 8, 5)]
-        vert_segments = [LineSegment(0, 0, 0, 7), LineSegment(6, 3, 6, 7)]
-
-        self.assertEqual(find_intersections(hor_segments, vert_segments), [(6, 5)])
+        wire1, wire2 = parse_puzzle_input(puzzle_input)
+        self.assertEqual(wire1, Wire(['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72']))
+        self.assertEqual(wire2, Wire(['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']))
 
     def test_solve_part_1_1(self):
-        test_input_1 = Path('tests/input/test_input_day03_01.txt').read_text()
-        self.assertEqual(solve_part_1(test_input_1), 6)
+        self.assertEqual(solve_part_1(self.test_input_1), 6)
 
     def test_solve_part_1_2(self):
-        test_input_1 = Path('tests/input/test_input_day03_02.txt').read_text()
-        self.assertEqual(solve_part_1(test_input_1), 159)
+        self.assertEqual(solve_part_1(self.test_input_2), 159)
 
     def test_solve_part_1_3(self):
-        test_input_1 = Path('tests/input/test_input_day03_03.txt').read_text()
-        self.assertEqual(solve_part_1(test_input_1), 135)
+        self.assertEqual(solve_part_1(self.test_input_3), 135)
+
+    def test_solve_part_2_1(self):
+        self.assertEqual(solve_part_2(self.test_input_1), 30)
+
+    def test_solve_part_2_2(self):
+        self.assertEqual(solve_part_2(self.test_input_2), 610)
+
+    def test_solve_part_2_3(self):
+        self.assertEqual(solve_part_2(self.test_input_3), 410)
 
 
 if __name__ == '__main__':
